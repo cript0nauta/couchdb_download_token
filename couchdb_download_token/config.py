a2863e105474a023114eca40f96e0f6fab4759f3
Matías Lang <yo@matiaslang.me>
2016-07-13 00:19:56 -0300

    Allow configurating token field name by environment variables.

--- couchdb_download_token/config.py
+++ couchdb_download_token/config.py
@@ -1,3 +1,4 @@
 import os
 COUCHDB_URL = os.environ.get('COUCHDB_URL', 'http://localhost:5984')
-DOWNLOAD_TOKEN_KEY_NAME = 'download_token'
+DOWNLOAD_TOKEN_KEY_NAME = os.environ.get('DOWNLOAD_TOKEN_KEY_NAME',
+                                         'download_token')
